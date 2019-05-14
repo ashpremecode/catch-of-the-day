7bc9fb10e873f92fd04eed76be69b670565332f6
ashpremcode <ashpremecode@gmail.com>
2019-05-14 16:01:10 +1000

    add function - load ad sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 
 class App extends React.Component {
 	constructor () {
 		super();
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
 		this.state = {
 			fishes: {},
 			order: {}
@@ -19,8 +21,14 @@ class App extends React.Component {
 		const timestamp = Date.now();
 		fishes[`fish-${timestamp}`] = fish;
 		this.setState ({ fishes })
-
 	}
+
+	loadSamples () {
+		this.setState ({
+			fishes: sampleFishes
+		});
+	}
+
 	render () {
 		return (
 			<div className="catch-of-the-day">
@@ -28,7 +36,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Food Market" />
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
