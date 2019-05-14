398ed334e1895055ae271b4e91c841396e65a693
ashpremcode <ashpremecode@gmail.com>
2019-05-14 14:48:17 +1000

    added go to store function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,31 @@
 import React from  'react';
 import {getFunName} from '../helpers';
+import { PropTypes } from 'prop-types';
 
 
 class StorePicker extends React.Component {
+
+	goToStore(event) {
+		event.preventDefault();
+		const storeId = this.storeInput.value;
+		this.context.router.history.push(`/store/${storeId}`);
+	}
+
+
+
 	render() {
 		return (
-			<form className="store-selector">
-			<h2>Please Enter A Store</h2>
-			<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-			<button type="submit">Vist Store</button>
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
+				<button type="submit">Vist Store</button>
 			</form>
 
 		)
 	}
 }
 
+StorePicker.contextTypes = {
+	router: PropTypes.object
+}
 export default StorePicker;
